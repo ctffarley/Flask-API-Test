14d543c4984af614d4836c8166e1a868ef43883a
Clark Farley <23282463+ctffarley@users.noreply.github.com>
2020-08-04 16:55:44 -0400

    updated README, changed api to use SQL

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask API Test
+
+## Purpose
+A basic Flask API that implements CRUD operations using Flask-RESTX and stores
+data in a PostgreSQL server. Interaction with the database is done using
+SQLAlchemy. Flask-RESTX also allows for the creation of swagger documentation
+for the API.
--- api.py
+++ api.py
@@ -1,78 +1,116 @@
 from flask import Flask, request
 from flask_restx import Resource, Api, fields
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
 api = Api(app)
 app.config["DEBUG"] = True
-# engine = create_engine('postgresql')
+
+db_string = 'postgresql://postgres:pass@127.0.0.1:5432/restaurant'
+db = create_engine(db_string)  
+base = declarative_base()
 
 item = api.model('Food', {
+    'id': fields.Integer(description = 'The food id'),
     'name': fields.String(required=True, description = 'The food name'),
     'price': fields.Integer(required=True, description = 'The food price')
 })
 
 ns = api.namespace('menu', description='menu CRUD operations')
 
-class Food(object):
-    def __init__(self, name, price):
-        self.name = name
-        self.price = price
+# class Food:
+#     def __init__(self, name, price, id):
+#         self.name = name
+#         self.price = price
+#         self.id = id
 
-    def update_price(self, price):
-        self.price = price
+#     def update_price(self, price):
+#         self.price = price
 
-class Menu(object):
-    def __init__(self):
-        self.items = []
+# class Menu:
+#     def __init__(self):
+#         self.items = []
+#         self.count = 0
 
-    def list_items(self):
-        return self.items
+#     def list_items(self):
+#         return self.items
 
-    def add_item(self, item):
-        self.items.append(item)
+#     def add_item(self, name, price):
+#         self.items.append(Food(name, price, self.count))
+#         self.count += 1
 
-    def remove_item(self, item):
-        self.items.remove(item)
+#     def remove_item(self, item):
+#         self.items.remove(item)
 
-    def update_price(self, item, price):
-        for i in self.items:
-            if i.name == item:
-                i.update_price(price)
+#     def update_price(self, id, price):
+#         for i in self.items:
+#             if i.id == id:
+#                 i.update_price(price)
 
-    def get_item(self, name):
-        for i in self.items:
-            if i.name == name:
-                return i
+#     def get_item(self, id):
+#         for i in self.items:
+#             if i.id == id:
+#                 return i
 
-menu = Menu()
 
-menu.add_item(Food('Burger', 10))
+# menu = Menu()
 
+# menu.add_item('Burger', 10)
+
+class Food(base):
+    __tablename__ = 'menuv2'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Integer)
+
+
+Session = sessionmaker(db)  
+session = Session()
+
+base.metadata.create_all(db)
+
+# root page that returns the full contents of the menu
 @ns.route('/')
 class MenuPage(Resource):
     @ns.marshal_list_with(item)
     def get(self):
-        return menu.items
+        return session.query(Food).all()
 
-@ns.route('/delete/<string:name>')
+# route allows you to delete an item based on id
+@ns.route('/delete/<int:id>')
 class DeleteItem(Resource):
-    def delete(self, name):
-        menu.remove_item(name)
+    def delete(self, id):
+        # query based on ID then remove that object
+        session.query(Food).filter(Food.id == id).delete()
 
+# This route supports post and put operations to create and update menu items,
+# respectively
 @ns.route('/item')
 class MenuItem(Resource):
     @api.doc(params={'name': 'name of the food', 'price': 'price of the food'})
     def post(self):
+        # get parameters from request
         name = request.args['name']
         price = request.args['price']
-        menu.add_item(Food(name, price))
 
-    @api.doc(params={'name': 'name of the food', 'price': 'price of the food'})
+        # add item to menu
+        temp = Food(name=name, price=price)
+        session.add(temp)
+        session.commit()
+
+    @api.doc(params={'id': 'id of the food item', 'price': 'price of the food'})
     def put(self):
-        name = request.args['name']
+        # get parameters from request
+        id = request.args['id']
         price = request.args['price']
-        menu.update_price(menu.get_item(name), price)
+
+        # TODO: query the db by id, then change the price
+        target = session.query(Food).filter(Food.id == id).first()
+        target.price = price
+        session.commit()
 
 if __name__ == '__main__':
     app.run()
